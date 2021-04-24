56c0b54afaefba2bd7f30710ae5968c49042bda4
bernardo <bernarrr69@gmail.com>
2021-04-24 02:04:10 +0200

    Minero crea cadena de bloques

--- p4/TO_DO.md
+++ p4/TO_DO.md
@@ -2,13 +2,31 @@ TO DO
 
 **MINERO** 
 1.  Minar con distintos trabajadores(hilos)   ✔️
-2.  Crear los bloques y almacenarlos 
-3.  Todo lo de la red de mineros que se hace con archivos compartidos
+2.  Crear los bloques y almacenarlos  ✔️
+3.  Crear monitor para que imprima la informacion ya que ahora lo hace el programa minero y esto no deberia ser asi 
+4.  Manejar la señal SIGUSR2 para detener ejecucion
+5.  
+6.  Todo lo de la red de mineros que se hace con archivos compartidos
 
+**MONITOR**
+1.  Crear proceso hijo  [fork]
+2.  Recibe nuevos bloques   [¿?]
+3.  El padre almacena los 10 ultimos bloques [array circular]
+4.  Padre al recibir mensaje sobre un bloque y comprueba solucion  [¿?]
+5.  Padre si el id no esta guardado borra el mas antiguo    [array circular]
+6.  Padre envia COPIA por TUBERIA de cada nuevo bloque al hijo [Toston de hacer por tuberia :upside_down_face:]
+7.  Hijo almacena TODOS los bloques         [La propia estructura bloque supongo]
+8.  Hijo imprime los bloques cada 5 secs    [bucle con sleep 5 supongo]
+9.  Finaliza al recibir SIGINT  [manejador de señales]
+10. REINICIAR???????? llamando de nuevo a ejecutarlo [¿?¿?¿?¿?¿ :upside_down_face: :exploding_head: ?¿?¿?¿?¿?]
 **RED**
 1. Esto es abrir secciones de mem compartida y que se acceda a ella desde mineros 
 2. Mirar todo lo de mensajes y demas
 
 **MIRAR EL RESTO CUANDO SEA NECESARIO**
 
+
+**DONE**
+
+1.  El minero multihilo capaz de generar una cadena de bloques  [2pts] :+1:
 
--- p4/minero.c
+++ p4/minero.c
@@ -7,74 +7,189 @@
 
 #define PRIME 99997669
 
-long int solucion = 0;
+long int solution = 0;
+int sol_found = 0;
 
-typedef struct worker_data{
+typedef struct worker_data
+{
     long int target;
     long int to_check;
-}Wd;
+    int cycles;
+} Wd;
 
-void * work_mine(void * arg){
-    Wd * wd;
+void *work_mine(void *arg)
+{
+    Wd *wd;
     wd = arg;
-    if(wd->target == simple_hash(wd->to_check)){
-        solucion = wd->to_check ;
-    }else{
-        wd->to_check++;
+    if (wd->cycles < 1)
+    {
+        while (sol_found == 0)
+        {
+            if (wd->target == simple_hash(wd->to_check))
+            {
+                solution = wd->to_check;
+                sol_found = 1;
+            }
+            else
+            {
+                wd->to_check = (wd->to_check + 1) % PRIME;
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < wd->cycles; i++)
+        {
+            if (wd->target == simple_hash(wd->to_check))
+            {
+                solution = wd->to_check;
+            }
+            else
+            {
+                wd->to_check = (wd->to_check + 1) % PRIME;
+            }
+        }
     }
 
     return NULL;
 }
 
+Block *create_new_block(Block *prev)
+{
+    Block *b = NULL;
+    if (!prev)
+    {
+        b = (Block *)malloc(sizeof(Block));
+        if (!b)
+        {
+            return NULL;
+        }
+        b->wallets[0] = 0;
+        b->target = 1;
+        b->solution = -1;
+        b->id = 1;
+        b->is_valid = 0;
+        b->prev = NULL;
+        b->next = NULL;
 
+        return b;
+    }
 
-int main(int argc, char **argv){
+    b = (Block *)malloc(sizeof(Block));
+    if (!b)
+    {
+        printf("error en la reserva de memoria del bloque\n");
+        return NULL;
+    }
+    b->wallets[0] = prev->wallets[0] + 1;
+    b->target = prev->solution;
+    b->solution = -1;
+    b->id = prev->id + 1;
+    b->is_valid = 0;
+    b->prev = prev;
+    b->next = NULL;
+
+    return b;
+}
+
+int main(int argc, char **argv)
+{
 
     int n_workers;
-    pthread_t *workers = NULL; 
-    Wd * workers_data = NULL;
-    int ciclo = 0;
+    int n_cycles;
+    pthread_t *workers = NULL;
+    Wd *workers_data = NULL;
+    Block *b;
+    Block *next;
 
-    if(argc != 3){
+    if (argc != 3)
+    {
         printf("Parametros de entrada erroneos\n");
         return -1;
     }
-    
+
     n_workers = atoi(argv[1]);
-    if(n_workers < 1){
+    if (n_workers < 1)
+    {
         printf("Numero de hilos menor que uno por favor introduzca un numero valido \n");
         return -1;
-    }else if(n_workers >MAX_WORKERS){
+    }
+    else if (n_workers > MAX_WORKERS)
+    {
         printf("Numero de hilos mayor que el maximo favor introduzca un numero valido \n");
         return -1;
     }
 
+    n_cycles = atoi(argv[2]);
+
     workers = malloc(sizeof(pthread_t) * n_workers);
-    if(!workers){
+    if (!workers)
+    {
         printf("ERROR al reservar memoria para los trabajadores");
         return -1;
     }
 
-    workers_data =(Wd *) malloc(sizeof(Wd)* n_workers);
-    if(!workers_data){
+    workers_data = (Wd *)malloc(sizeof(Wd) * n_workers);
+    if (!workers_data)
+    {
         free(workers);
         return -1;
     }
 
-    
     long section = PRIME / n_workers;
-
-    for(int i = 0 ; i < n_workers ; i++){
-        workers_data[i].target = 1; // cambiarlo por el target buscado par cada caso
-        workers_data[i].to_check = section * i;
+    /* CREACION DEL PRIMER BLOQUE */
+    b = create_new_block(NULL);
+    if (!b)
+    {
+        printf("error en la reserva de memoria del bloque");
+        free(workers);
+        free(workers_data);
+        return -1;
     }
-    while(solucion == 0){
-        for(int i = 0; i < n_workers; i++){
-            pthread_create(&workers[i],NULL, work_mine, &workers_data[i]);
+
+    /* Repeticion un numero cualquiera de veces para comprobar si se crean bien todos los bloques */
+    for (int a = 0; a < 10; a++)
+    {
+        for (int i = 0; i < n_workers; i++)
+        {
+            workers_data[i].target = b->target; // cambiarlo por el target buscado par cada caso
+            workers_data[i].to_check = section * i;
+            workers_data[i].cycles = n_cycles;
+        }
+
+        for (int i = 0; i < n_workers; i++)
+        {
+            pthread_create(&workers[i], NULL, work_mine, &workers_data[i]);
+        }
+        for (int i = 0; i < n_workers; i++)
+        {
             pthread_join(workers[i], NULL);
         }
-        printf("ciclo %d \r", ciclo++);
-    }
-    printf("SOLUCION == %ld", solucion);
 
+        if (sol_found)
+        {
+            /* actualizo la solucion */
+            b->solution = solution;
+            b->is_valid = 1;
+            /* Creo el siguiente bloque */
+            next = create_new_block(b);
+            if (!next)
+            {
+                printf("error en la reserva de memoria del bloque");
+                free(workers);
+                free(workers_data);
+                return -1;
+            }
+            /* Establezco el siguiente bloque */
+            b->next = next;
+            /* Y apunto al siguiente bloque*/
+            b = b->next;
+
+            sol_found = 0;
+        }
+    }
+
+    print_blocks(b, 1);
+
+    exit(EXIT_SUCCESS);
 }
